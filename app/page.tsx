71d82a5755bbd1931b039c9ed66a2d2c530874f2
moathssnck <myneww00@gmail.com>
2025-06-30 15:04:21 +0300

    سس

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import ZainPaymentInterface from "@/components/z-form";
 import ZainPayment from "@/components/zain-payment";
 
 export default function Page() {
-  return <ZainPaymentInterface />;
+  return <ZainPayment />;
 }
